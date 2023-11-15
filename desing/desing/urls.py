5392f2558524bf6d15c5df5dda732a106d8e8612
vlukashh <ankalukashenko@gmail.ru>
2023-11-15 20:29:42 +0700

    сделана структура папок, главная страница и шаблоны для авторизации

---
+++ desing/desing/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include
+from django.contrib import admin
+from django.urls import path
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
+    path('accounts/', include('django.contrib.auth.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
